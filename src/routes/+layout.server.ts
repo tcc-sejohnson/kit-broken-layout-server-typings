dde33ce9f261e052b27cbd997e5bccb499c8703b
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-08-17 01:09:00 -0600

    fix: Comment to defend my laziness

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,8 @@ import type { LayoutServerLoad } from './$types';
 import type { PublicSessionData } from './types';
 
 export const load: LayoutServerLoad = () => {
+	// this is lazy, but the error exists even when you build out a full object
+	// instead of typecasting
 	const session: PublicSessionData = {} as PublicSessionData;
 	return {
 		session
